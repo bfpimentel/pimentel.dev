597c76578d45fc6196558ffa839a51cb8b39d4b3
Bruno Pimentel <bruno@pimentel.dev>
2021-04-11 17:55:20 -0300

    feat: move posts component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import styles from "@/styles/Header.module.css";
 
 function Header() {
-  return <h1 className={styles.title}>Bruno Pimentel</h1>;
+  return (
+    <>
+      <img
+        className={styles.photo}
+        src="https://github.com/bfpimentel.png?size=200"
+        alt="Profile Picture"
+      />
+      <h2 className={styles.title}>Bruno Pimentel</h2>
+      <h3 className={styles.short_description}>
+        Self-taught. Adaptable. Challenge seeker.
+      </h3>
+    </>
+  );
 }
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Header from "@/components/Header";
+import Posts from "@/components/Posts";
 import styles from "@/styles/Home.module.css";
 
 function Home() {
@@ -13,38 +14,7 @@ function Home() {
 
       <main className={styles.main}>
         <Header />
-
-        <p className={styles.description}>Last Posts</p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Posts />
       </main>
 
       <footer className={styles.footer}>
