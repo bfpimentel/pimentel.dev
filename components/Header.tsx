af6d8c3d9a2d4b7e0a7f3f7f52ab63571c8db91f
Bruno Pimentel <bruno@pimentel.dev>
2021-05-14 14:19:54 -0300

    feat: add more info and fix hovers

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,10 @@ export default function Header() {
         alt="Profile Picture"
       />
       <h2 className="m-3 leading-5 text-4xl font-bold">Bruno Pimentel</h2>
-      <h3 className="m-3 text-center text-2xl">Senior Android Engineer.</h3>
+      <h3 className="m-3 text-center text-2xl">
+        Senior Android Engineer
+        <br />@ Cornershop by Uber
+      </h3>
       <h3 className="mb-5 text-center text-2xl">
         Self-taught. Adaptable. Challenge seeker.
       </h3>
