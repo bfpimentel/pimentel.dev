758378c77e2e9c7c7d80ded18f4191e2d3b5fa5e
Bruno Pimentel <bruno@pimentel.dev>
2022-08-05 14:52:20 -0300

    fix: use of env variable within query

--- data/source/GithubDataSource.ts
+++ data/source/GithubDataSource.ts
@@ -2,40 +2,43 @@ import { gql } from "@apollo/client";
 import client from "@/data/apollo";
 
 export const getPinnedRepositories = async () => {
-  const { data } = await client.query({
-    query: gql`
-      query PinnedRepositories {
-        user(login: "bfpimentel") {
-          pinnedItems(first: 6, types: REPOSITORY) {
-            nodes {
-              ... on Repository {
-                name
-                description
-              }
+  const GET_PINNED_REPOSITORIES_QUERY = gql`
+    query PinnedRepositories {
+      user(login: "bfpimentel") {
+        pinnedItems(first: 6, types: REPOSITORY) {
+          nodes {
+            ... on Repository {
+              name
+              description
             }
           }
         }
       }
-    `,
-  });
+    }
+  `;
 
+  const { data } = await client.query({ query: GET_PINNED_REPOSITORIES_QUERY });
+  
   return data;
 };
 
-export const getShortenedURLsGist = async () => { 
-  const { data } = await client.query({
-    query: gql`
-      query ShortenedURLsGist {
-        user(login: "bfpimentel") {
-          gist(name: ${process.env.SHORTENED_URLS_GIST_NAME}) {
-            description
-            files(limit: 1) {
-              text
-            }
+export const getShortenedURLsGist = async () => {  //Dog($breed: String!)
+  const GET_SHORTENED_URLS_GIST_QUERY = gql`
+    query ShortenedURLsGist($gistName: String!) {
+      user(login: "bfpimentel") {
+        gist(name: $gistName) {
+          description
+          files(limit: 1) {
+            text
           }
         }
       }
-    `,
+    }
+  `;
+
+  const { data } = await client.query({
+    query: GET_SHORTENED_URLS_GIST_QUERY,
+    variables: { "gistName": process.env.SHORTENED_URLS_GIST_NAME },
   });
 
   return data;
