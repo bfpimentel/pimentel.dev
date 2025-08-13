d72121aa8501679ab96e24b8833b7baa5e0e3dbb
Bruno Pimentel <hello@bruno.so>
2025-08-13 21:03:15 +0200

    chore: use external API for fetching pinned repos

--- src/data/repository/GithubRepository.ts
+++ src/data/repository/GithubRepository.ts
@@ -1,5 +1,3 @@
-import { HTMLElement, parse } from "node-html-parser"
-
 import type ProfileModel from "@/data/model/ProfileModel"
 import type ShortenedURLModel from "@/data/model/ShortenedURLModel"
 
@@ -25,14 +23,15 @@ interface RepositoryResponse {
 export async function getGithubProfile(): Promise<ProfileModel> {
   return fetch(GET_PROFILE_URL).then(async (profileResponse) => {
     const parsedProfile = (await profileResponse.json()) as ProfileResponse
-    const pinnedRepositories = await getPinnedRepositories()
+    const pinnedRepositories = await fetch("https://pinned.berrysauce.dev/get/bfpimentel")
+    const parsedPinnedRepositories = (await pinnedRepositories.json()) as RepositoryResponse[]
 
     return {
       name: parsedProfile.name,
       photoUrl: parsedProfile.avatar_url,
       bio: parsedProfile.bio,
       role: parsedProfile.company,
-      projects: pinnedRepositories.map((repo) => ({
+      projects: parsedPinnedRepositories.map((repo) => ({
         name: repo.name,
         description: repo.description,
         link: `https://github.com/bfpimentel/${repo.name}`,
@@ -55,46 +54,4 @@ export async function getShortenedURLs(): Promise<ShortenedURLModel[]> {
   //
   //   return shortenedURL
   // })
-}
-
-function parseRepository(el: HTMLElement): RepositoryResponse {
-  const repoPath = el.querySelector("a")?.getAttribute("href")?.split("/") || []
-  const [, author = "", name = ""] = repoPath
-
-  const parseMetric = (index: number): number => {
-    try {
-      return Number(el.querySelectorAll("a.pinned-item-meta")[index]?.text?.replace(/\n/g, "").trim()) || 0
-    } catch {
-      return 0
-    }
-  }
-
-  const languageSpan = el.querySelector("span[itemprop='programmingLanguage']")
-  const languageColorSpan = languageSpan?.parentNode?.querySelector(".repo-language-color")
-
-  return {
-    author,
-    name,
-    description: el.querySelector("p.pinned-item-desc")?.text?.replace(/\n/g, "").trim() || "",
-    language: languageSpan?.text || "",
-    languageColor: languageColorSpan?.getAttribute("style")?.match(/background-color:\s*([^;]+)/)?.[1] || "",
-    stars: parseMetric(0),
-    forks: parseMetric(1),
-  }
-}
-
-async function getPinnedRepositories(): Promise<RepositoryResponse[]> {
-  try {
-    const request = await fetch("https://github.com/bfpimentel", {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    })
-    const html = await request.text()
-    const root = parse(html)
-
-    return root.querySelectorAll(".js-pinned-item-list-item").map((el) => parseRepository(el))
-  } catch {
-    return []
-  }
 }
